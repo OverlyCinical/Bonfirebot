7c5a0282037c2515b80b376200fc248803308ed8
OverlyCynical <wade.dunn2020@gmail.com>
2017-05-11 18:31:25 -0700

    implementation of self joinable roles
    In addition to adding the ability to create joinable roles outside of
    hardcoding them, this update also seeks to clean up the role joining
    code in general. in preperation for this update I changed the name of
    "testStr" to "gameList".

--- MyBot.cs
+++ MyBot.cs
@@ -21,14 +21,14 @@ namespace ConsoleApplication1
         string[] youDiedGifs = Directory.GetFiles(@"C:\Users\Wade\Documents\Visual Studio 2015\Projects\ConsoleApplication1\ConsoleApplication1\YouDied");
         string[] smugPic = Directory.GetFiles(@"C:\Users\Wade\Documents\Visual Studio 2015\Projects\ConsoleApplication1\ConsoleApplication1\smug");
 
-        string[] testStr;
+        string[] gameList;
 
         public MyBot()
         {
             rand = new Random();
 
 
-            testStr = new string[]
+            gameList = new string[]
             {
                 "overwatch",
                 "dontstarve",
@@ -67,6 +67,12 @@ namespace ConsoleApplication1
                 await SendMessage(e);
             });
 
+            commands.CreateCommand("agc").Parameter("channel", ParameterType.Multiple)
+            .Do(async (e) =>
+            {
+                await AddGameChannel(e);
+            });
+
             RegisterYouDiedCommand();
             RegisterSmugCommand();
             RegisterKindleCommand();
@@ -99,7 +105,6 @@ namespace ConsoleApplication1
             commands.CreateCommand("smug")
             .Do(async (e) =>
             {
-                    //await e.Channel.SendMessage("pong");
                     int randomInt = rand.Next(smugPic.Length);
                     string imageToPost = smugPic[randomInt];
                     await e.Channel.SendFile(imageToPost);
@@ -264,6 +269,25 @@ namespace ConsoleApplication1
 
             var result = name + " says: " + message;
             return result;
+        }
+
+        private async Task AddGameChannel(CommandEventArgs e)
+        {
+            var userRoles = e.User.Roles;
+            if (userRoles.Any(discord => discord.Name.ToUpper() == "FIREKEEPER"))
+            {
+                var channel = e.Server.FindChannels("botchan", ChannelType.Text).FirstOrDefault();
+                string game = "";
+                for (int i = 0; i < e.Args.Length; i++)
+                {
+                    game += e.Args[i].ToString() + " ";
+                }
+                await channel.SendMessage("test" + e.Args);
+            }
+            else
+            {
+                await e.Channel.SendMessage("```You do not have permission to use this command...```");
+            }
         }
 
         private void Log(object sender, LogMessageEventArgs e)
